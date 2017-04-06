5afe577ba4de334ed006db2e4d838ced4f9fb8af
quentin <quentincperso@gmail.com>
2017-04-06 22:39:41 +0200

    renamed everything

--- mysql/sigyn.sql
+++ mysql/sigyn.sql
@@ -5,7 +5,7 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-USE `mydoctor`;
+USE `sigyn`;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
--- sigyn/www/app/models/User.php
+++ sigyn/www/app/models/User.php
@@ -59,7 +59,7 @@ class User extends \Phalcon\Mvc\Model
      */
     public function initialize()
     {
-        $this->setSchema("mydoctor");
+        $this->setSchema("sigyn");
     }
 
     /**
