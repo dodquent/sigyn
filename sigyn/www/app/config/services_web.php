39c70ca622a5abcbd6e8aef79deb7d3786da335b
Thomas LE CORRE <thomas.lecorre@yahoo.fr>
2017-04-07 21:13:09 +0200

    cache and plugins

--- sigyn/www/app/config/services_web.php
+++ sigyn/www/app/config/services_web.php
@@ -7,6 +7,7 @@ use Phalcon\Session\Adapter\Files as SessionAdapter;
 use Phalcon\Mvc\View;
 use Phalcon\Mvc\View\Engine\Volt as VoltEngine;
 use Phalcon\Flash\Direct as Flash;
+use Phalcon\Events\Manager as EventsManager;
 
 /**
  * Registering a router
@@ -53,11 +54,38 @@ $di->set('flash', function () {
     ]);
 });
 
+
+$di->set('flashSession', function() {
+    return new \Phalcon\Flash\Session(array(
+        'error' => 'alert alert-danger',
+        'success' => 'alert alert-success',
+        'notice' => 'alert alert-info',
+        'warning' => 'alert alert-warning'
+    ));
+}, true);
+
 /**
-* Set the default namespace for dispatcher
-*/
-$di->setShared('dispatcher', function() {
-    $dispatcher = new Dispatcher();
-    $dispatcher->setDefaultNamespace('Sigyn\Modules\Frontend\Controllers');
-    return $dispatcher;
-});
+ * Events manager
+ */
+$di->setShared(
+    "dispatcher",
+    function () {
+        // Create an events manager
+        $eventsManager = new EventsManager();
+        // Listen for events produced in the dispatcher using the Security plugin
+        $eventsManager->attach(
+            "dispatch:beforeExecuteRoute",
+            new SecurityPlugin()
+        );
+        // Handle exceptions and not-found exceptions using NotFoundPlugin
+        $eventsManager->attach(
+            "dispatch:beforeException",
+            new NotFoundPlugin()
+        );
+        $dispatcher = new Dispatcher();
+        $dispatcher->setDefaultNamespace('Sigyn\Modules\Frontend\Controllers');
+        // Assign the events manager to the dispatcher
+        $dispatcher->setEventsManager($eventsManager);
+        return $dispatcher;
+    }
+);
