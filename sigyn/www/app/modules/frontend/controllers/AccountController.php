c22d429ce13e22be6dec617e94919fb8c58cd376
Thomas LE CORRE <thomas.lecorre@yahoo.fr>
2017-04-08 15:08:40 +0200

    mailer

--- sigyn/www/app/modules/frontend/controllers/AccountController.php
+++ sigyn/www/app/modules/frontend/controllers/AccountController.php
@@ -2,14 +2,15 @@
 
 namespace Sigyn\Modules\Frontend\Controllers;
 
+use Phalcon\Mvc\Url;
+
 class AccountController extends ControllerBase
 {
     public function forgottenPasswordAction()
     {
-        var_dump($this->request->getPost("emailRecovery"));die;
         if ($this->request->isPost())
         {
-            $email = $this->request->getPost("emailRecovery");
+            $email = strtolower($this->request->getPost("emailRecovery"));
             if (empty($email))
             {
                 $this->flash->error("Please enter a valid email address");
@@ -20,10 +21,34 @@ class AccountController extends ControllerBase
                     ]
                 );
             }
+            $token = bin2hex(openssl_random_pseudo_bytes(16));
+            $link = $this->request->getServerName() . "/account/validToken/$token";
+
             // ENVOI DU MAIL
+            $mail = $this->mailer;
+            $mail->Subject = "Test";
+            $content = file_get_contents(APP_PATH . '/modules/frontend/mail/template_forgottenPassword.html');
+            $mail->Body = str_replace('{link}', $link, $content);
+            $mail->AddAddress($email);
+
+            if(!$mail->Send()) {
+                $this->flash->error("Mailer Error: " . $mail->ErrorInfo);
+                return $this->dispatcher->forward(
+                    [
+                        "controller" => "session",
+                        "action"     => "index",
+                    ]
+                );
+            }
             
+            $this->flashSession->notice("An email as been sent to $email");
         }
         return $this->response->redirect("index");
     }
+
+    public function validTokenAction($token)
+    {
+        var_dump($token);die;
+    }   
 
 }
