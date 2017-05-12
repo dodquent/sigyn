78d9f4b1df1edcf4f8438a2ea0fd9b788939e9f6
quentin <quentincperso@gmail.com>
2017-05-12 16:42:25 +0200

    get pro

---
+++ pros/get.py
@@ -0,0 +1,22 @@
+import os
+import json
+from tools import decimalencoder
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+def get(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    result = table.get_item(
+        Key={
+            'email': event['queryStringParameters']['email']
+        }
+    )
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Item'], cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
+
