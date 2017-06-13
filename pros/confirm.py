62683dc10dc83a13df8691287b1404952199953f
quentin <quentincperso@gmail.com>
2017-06-13 10:39:47 +0200

    updated confirm

--- pros/confirm.py
+++ pros/confirm.py
@@ -4,37 +4,32 @@ import time
 import uuid
 
 import boto3
-from botocore.exceptions import ClientError
 cognito = boto3.client('cognito-idp', region_name='eu-west-1')
 
-def create(event, context):
+def confirm(event, context):
     data = json.loads(event['body'])
 
-    if 'email' not in data or 'name' not in data:
+    if 'code' not in data or 'username' not in data:
         response = {
             "statusCode": 400,
             "body": "Missing parameters",
         }
         return response
 
+    code = data['code']
+    username = data['username']
+
     try:
-        res = cognito.sign_up(
+        res = cognito.confirm_sign_up(
             ClientId=os.environ['COGNITO_ID'],
-            Username=data['name'],
-            Password=data['password'],
-            UserAttributes=[
-                {
-                    'Name': 'email',
-                    'Value': data['email']
-                }
-            ]
+            Username=username,
+            ConfirmationCode=code
         )
     except Exception as e:
         res = str(e)
     response = {
         "statusCode": 200,
         "body": json.dumps(res)
-    }
-
+        }
     return response
 
