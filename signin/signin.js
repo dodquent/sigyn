440555e69ffc4e8fc1d0be123810860182f4e382
quentin <quentincperso@gmail.com>
2017-06-15 17:27:26 +0200

    sign in with parameters

--- signin/signin.js
+++ signin/signin.js
@@ -3,46 +3,50 @@ var AWSCognito = require('amazon-cognito-identity-js-node');
 
 module.exports.signin = (event, context, callback) => {
 
-	var authenticationData = {
-		Username: 'thomas',
-		Password: 'Toto1234@'
-	};
-	var authenticationDetails = new AWSCognito.AuthenticationDetails(authenticationData);
+    const data = JSON.parse(event.body);
 
-	var poolData = {
-		UserPoolId: 'eu-west-1_QrzGaubhy',
-		ClientId: '4l0luscskuf56rqkeh7er7lje6'
-	};
-	var userPool = new AWSCognito.CognitoUserPool(poolData);
+    var authenticationData = {
+        Username: data.name,
+        Password: data.password
+    };
+    var authenticationDetails = new AWSCognito.AuthenticationDetails(authenticationData);
 
-	var userData = {
-		Username: 'thomas',
-		Pool: userPool
-	};
+    var poolData = {
+        UserPoolId: 'eu-west-1_QrzGaubhy',
+        ClientId: '4l0luscskuf56rqkeh7er7lje6'
+    };
+    var userPool = new AWSCognito.CognitoUserPool(poolData);
 
-	var cognitoUser = new AWSCognito.CognitoUser(userData);
-	cognitoUser.authenticateUser(authenticationDetails, {
-		onSuccess: function (result) {
-			console.log('access token + ' + result.getAccessToken().getJwtToken());
-			/*Use the idToken for Logins Map when Federating User Pools with Cognito Identity or when passing through an Authorization Header to an API Gateway Authorizer*/
-			console.log('idToken + ' + result.idToken.jwtToken);
-			const response = {
-				statusCode: 200,
-				body: JSON.stringify({
-					message: result.idToken.jwtToken,
-					input: event,
-				}),
-			};
-			callback(null, response);
-		},
+    var userData = {
+        Username: data.name,
+        Pool: userPool
+    };
 
-		onFailure: function (err) {
-			console.log(err);
-		},
+    var cognitoUser = new AWSCognito.CognitoUser(userData);
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function(result) {
+            console.log('access token + ' + result.getAccessToken().getJwtToken());
+            console.log('idToken + ' + result.idToken.jwtToken);
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: result.idToken.jwtToken,
+                    input: event,
+                })
+            };
+            callback(null, response);
+        },
 
-	});
-
-
-	// Use this code if you don't use the http event with the LAMBDA-PROXY integration
-	// callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+        onFailure: function(err) {
+            console.log(err);
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: err,
+                    input: event,
+                })
+            };
+            callback(null, response);
+        },
+    });
 };
